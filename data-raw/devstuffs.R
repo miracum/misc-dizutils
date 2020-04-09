7d31d706794826303709d814fdcf03e188cda36c
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2020-04-09 10:08:58 +0200

    first commit

---
+++ data-raw/devstuffs.R
@@ -0,0 +1,144 @@
+packagename <- "DIZutils"
+
+# remove existing description object
+unlink("DESCRIPTION")
+
+# Create a new description object
+my_desc <- desc::description$new("!new")
+
+# Set your package name
+my_desc$set("Package", packagename)
+
+# Set author names 2
+# http://r-pkgs.had.co.nz/description.html
+# Full list of roles: http://www.loc.gov/marc/relators/relaterm.html
+my_desc$set_authors(c(
+  person(
+    "Lorenz A.",
+    "Kapsner",
+    email = "lorenz.kapsner@uk-erlangen.de",
+    role = c("cre", "aut"),
+    comment = c(ORCID = "0000-0003-1866-860X")
+  ),
+  person("Jonathan M.", "Mang", role = c("aut"),
+         comment = c(ORCID = "0000-0003-0518-4710")),
+  person("MIRACUM - Medical Informatics in Research and Care in University Medicine", role = c("fnd"))
+))
+
+# Set copyright
+my_desc$set("Copyright", "Universitätsklinikum Erlangen")
+
+# Remove some author fields
+my_desc$del("Maintainer")
+
+# Set the version
+my_desc$set_version("0.0.0.9000")
+
+# The title of your package
+my_desc$set(Title = "Utilities for DIZ R package development")
+
+# The description of your package
+my_desc$set(Description = "The package provides utilities functions used in DIZ R package development.")
+
+# The date when description file has been created
+my_desc$set("Date" = as.character(Sys.Date()))
+
+# The urls
+my_desc$set("URL", "https://gitlab.miracum.org/miracum/dqa/dizutils")
+my_desc$set("BugReports", "https://gitlab.miracum.org/miracum/dqa/dizutils/issues")
+
+# Vignette Builder
+my_desc$set("VignetteBuilder" = "knitr")
+
+# License
+my_desc$set("License", "GPL-3")
+
+# for BioConductor stuff
+# see: https://bioinformatics.stackexchange.com/questions/3365/r-package-development-how-does-one-automatically-install-bioconductor-packages
+# and: https://github.com/r-lib/devtools/issues/700
+my_desc$set("biocViews" = "")
+
+# Save everyting
+my_desc$write(file = "DESCRIPTION")
+
+# License
+usethis::use_gpl3_license(name="Universitätsklinikum Erlangen")
+
+
+# add Imports and Depends
+# Listing a package in either Depends or Imports ensures that it’s installed when needed
+# Imports just loads the package, Depends attaches it
+# Loading will load code, data and any DLLs; register S3 and S4 methods; and run the .onLoad() function.
+##      After loading, the package is available in memory, but because it’s not in the search path,
+##      you won’t be able to access its components without using ::.
+##      Confusingly, :: will also load a package automatically if it isn’t already loaded.
+##      It’s rare to load a package explicitly, but you can do so with requireNamespace() or loadNamespace().
+# Attaching puts the package in the search path. You can’t attach a package without first loading it,
+##      so both library() or require() load then attach the package.
+##      You can see the currently attached packages with search().
+
+# Depends
+usethis::use_package("R", min_version = "2.10", type = "Depends")
+
+# Imports
+usethis::use_package("data.table", type="Imports")
+usethis::use_package("shiny", type="Imports")
+usethis::use_package("ggplot2", type="Imports")
+usethis::use_package("magrittr", type="Imports")
+usethis::use_package("polynom", type="Imports")
+usethis::use_package("ggpubr", type="Imports")
+
+# Suggests
+usethis::use_package("testthat", type = "Suggests")
+usethis::use_package("lintr", type = "Suggests")
+# for vignettes
+usethis::use_package("rmarkdown", type = "Suggests")
+usethis::use_package("qpdf", type = "Suggests")
+usethis::use_package("knitr", type = "Suggests")
+
+
+# buildignore
+usethis::use_build_ignore("LICENSE.md")
+usethis::use_build_ignore(".gitlab-ci.yml")
+usethis::use_build_ignore("data-raw")
+usethis::use_build_ignore(".vscode")
+usethis::use_build_ignore(".lintr")
+
+# gitignore
+usethis::use_git_ignore("/*")
+usethis::use_git_ignore("/*/")
+usethis::use_git_ignore("*.log")
+usethis::use_git_ignore("!/.gitignore")
+usethis::use_git_ignore("!/.Rbuildignore")
+usethis::use_git_ignore("!/.gitlab-ci.yml")
+usethis::use_git_ignore("!/data-raw/")
+usethis::use_git_ignore("!/DESCRIPTION")
+usethis::use_git_ignore("!/inst/")
+usethis::use_git_ignore("!/LICENSE.md")
+usethis::use_git_ignore("!/man/")
+usethis::use_git_ignore("!NAMESPACE")
+usethis::use_git_ignore("!/R/")
+usethis::use_git_ignore("!/README.md")
+usethis::use_git_ignore("!/tests/")
+usethis::use_git_ignore("/.Rhistory")
+usethis::use_git_ignore("!/*.Rproj")
+usethis::use_git_ignore("/.Rproj*")
+usethis::use_git_ignore("/.RData")
+usethis::use_git_ignore("/.vscode")
+usethis::use_git_ignore("!/.lintr")
+
+# code coverage
+# covr::package_coverage()
+
+# lint package
+# lintr::lint_package()
+
+# test package
+# devtools::test()
+
+# R CMD check package
+# rcmdcheck::rcmdcheck()
+# rcmdcheck::rcmdcheck(args = "--no-vignettes", build_args = "--no-build-vignettes")
+
+# tidy description
+usethis::use_tidy_description()
