2941a6eaae92b5073b63dd4e5abe732e13afbce0
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-05-05 14:34:00 +0200

    chore: updated devstuffs to get auto-changelog working again

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -201,6 +201,12 @@ usethis::use_tidy_description()
 # create NEWS.md using the python-package "auto-changelog" (must be installed)
 # https://www.conventionalcommits.org/en/v1.0.0/
 # build|ci|docs|feat|fix|perf|refactor|test
+
+# https://github.com/gitpython-developers/GitPython/issues/1016#issuecomment-1104114129
+system(
+  command = paste0("git config --global -add safe.directory ", getwd())
+)
+
 system(command = paste0(
   'auto-changelog -u -t "',
   packagename,
